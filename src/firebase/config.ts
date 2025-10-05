de1fc0fa805be6b0b0fd432e3169d85c98e68a9b
Sambody Inc <sambodyinc@gmail.com>
2025-10-05 20:00:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3915944121-b968c",
+  "appId": "1:481934396191:web:915387ee819b94e88d7e2a",
+  "apiKey": "AIzaSyDTHWyP08NVa5wPAiUXOEB0sg7b9RyqFNg",
+  "authDomain": "studio-3915944121-b968c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "481934396191"
+};
